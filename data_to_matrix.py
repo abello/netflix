2cfe8211f0f9a0735803b5b41b60415557d82f9c
Dae Hyun Kim <dhkim0106@gmail.com>
2014-04-11 13:01:57 -0700

    Attempt to use data to make sparse matrix
    - Attempted to use data.npz to make a sparse matrix & serialize
    
    TODO:
    - Seems to be inefficient / malfunctional. Needs fix

---
+++ data_to_matrix.py
@@ -0,0 +1,23 @@
+from scipy import sparse
+import numpy as np
+
+# Number of users in all.dta
+NUM_USERS = 458293
+
+# Number of movies
+NUM_MOVIES = 17770
+
+f = np.load(open("data.npz", "r"))
+data = f["arr_0"]
+
+mat = sparse.lil_matrix((NUM_USERS, NUM_MOVIES),  dtype=np.float16);
+for usr in xrange(NUM_USERS):
+	for i in xrange(len(data[usr]) / 2):
+		mat[usr, data[usr][i * 2] - 1] = data[usr][i * 2 + 1]
+	if usr % 10000 == 0:
+		print(usr);
+
+
+
+print("DONE");
+np.savez(open("mat.npz", "w"), mat)

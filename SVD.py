4cda68681d12200faf9279d632bcc3c93a31300e
Dae Hyun Kim <dhkim0106@gmail.com>
2014-04-13 16:37:33 -0700

    SVD Starting Piece Debugged I
    - Debugged minor issues
    - Implemented improved movie average computation
    
    TODO:
    
    - Finish & Further debug

--- SVD.py
+++ SVD.py
@@ -8,26 +8,6 @@ NUM_USERS = 458293;
 # Number of movies
 NUM_MOVIES = 17770;
 
-print("Reading from data.npz...");
-
-f = np.load(open("data.npz", "r"));
-data = f["arr_0"];
-
-print("Finished reading from data.npz!");
-
-user_features = [None for i in xrange(NUM_FEATURES)];
-
-movie_features = [None for i in xrange(NUM_FEATURES)];
-
-for f in xrange(NUM_FEATURES):
-	user_features[f] = [None for i in xrange(NUM_USERS)];
-	movie_features[f] = [None for i in xrange(NUM_MOVIES)];
-
-global_avg = compute_global_avg(data);
-
-user_offset = compute_user_offset(data, global_avg);
-
-movie_avg = compute_movie_avg(data);
 
 def compute_global_avg(data):
 	print("Computing the global average...");
@@ -51,7 +31,7 @@ def compute_user_avg(data):
 	user_avg = [None for i in xrange(NUM_USERS)];
 
 	for u in xrange(NUM_USERS):
-		user_cnt += len(user_data) / 2;
+		user_cnt = len(data[u]) / 2;
 		user_sum = 0;
 		
 		for i in xrange(len(data[u]) / 2):
@@ -84,9 +64,51 @@ def compute_movie_avg(data):
 
 def compute_user_offset(data, global_avg):
 	user_avg = compute_user_avg(data);
-	user_offset = [(usr_avg[i] - global_avg) for i in xrange(NUM_USERS)];
+	user_offset = [(user_avg[i] - global_avg) for i in xrange(NUM_USERS)];
 	return user_offset;
+
+def improved_movie_avg(data, global_avg, K = 25):
+	print("Computing the movie averages (improved)...");
+
+	movie_avg = [None for i in xrange(NUM_MOVIES)];
+	movie_cnt = [K * global_avg for i in xrange(NUM_MOVIES)];
+	movie_sum = [K for i in xrange(NUM_MOVIES)];
+
+	for u in xrange(NUM_USERS):
+		for i in xrange(len(data[u]) / 2):
+			movie_no = data[u][2 * i] - 1; # shift by 1!
+			movie_cnt[movie_no] += 1;
+			movie_sum[movie_no] += data[u][2 * i + 1];
+
+	for m in xrange(NUM_MOVIES):
+		movie_avg[m] = movie_sum[m] / movie_cnt[m];
+
+	print("Finished computing the movie averages!");
+	return movie_avg;
+
 
 # base value for predicted ratings
 def predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id):
 	return movie_avg[movie_id] + user_offset[user_id];
+
+
+print("Reading from data.npz...");
+
+f = np.load(open("data.npz", "r"));
+data = f["arr_0"];
+
+print("Finished reading from data.npz!");
+
+user_features = [None for i in xrange(NUM_FEATURES)];
+
+movie_features = [None for i in xrange(NUM_FEATURES)];
+
+for f in xrange(NUM_FEATURES):
+	user_features[f] = [None for i in xrange(NUM_USERS)];
+	movie_features[f] = [None for i in xrange(NUM_MOVIES)];
+
+global_avg = compute_global_avg(data);
+
+user_offset = compute_user_offset(data, global_avg);
+
+movie_avg = compute_movie_avg(data);

b66a1af3a8660da59be90f8bddf8f2d8a0e123eb
Dae Hyun Kim <dhkim0106@gmail.com>
2014-04-13 17:19:53 -0700

    SVD Backbone Done
    - Completed backbone of SVD algorithm
    
    TODO:
    - Debug for any issues
    - Printing functions

--- SVD.py
+++ SVD.py
@@ -3,10 +3,14 @@ import numpy as np
 NUM_FEATURES = 40;
 
 # Number of users in all.dta
-NUM_USERS = 458293;
+NUM_USERS = 100;#458293;
 
 # Number of movies
 NUM_MOVIES = 17770;
+
+NUM_ITERATIONS = 10;
+
+LEARNING_RATE = 0.001;
 
 
 def compute_global_avg(data):
@@ -91,6 +95,24 @@ def improved_movie_avg(data, global_avg, K = 25):
 def predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id):
 	return movie_avg[movie_id] + user_offset[user_id];
 
+def predict_rating(user_features, movie_features, user_offset, movie_avg, user_id, movie_id, rating_initialized):
+	if not rating_initialized:
+		return predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id);
+	else:
+		tot_sum = 0;
+		for f in xrange(NUM_FEATURES):
+			tot_sum += user_features[f][user_id] * movie_features[f][movie_id];
+		return tot_sum;
+
+def train(user_features, movie_features, f, user_offset, movie_avg, user_id, movie_id, rating_initialized, actual_rating, learning_rate):
+	error = learning_rate * (actual_rating - predict_rating(user_features, movie_features, user_offset, movie_avg, user_id, movie_id, rating_initialized));
+	uv_old = user_features[f][user_id];
+	user_features[f][user_id] += error * movie_features[f][movie_id];
+	movie_features[f][movie_id] += error * uv_old;
+	return;
+
+
+
 
 print("Reading from data.npz...");
 
@@ -104,11 +126,30 @@ user_features = [None for i in xrange(NUM_FEATURES)];
 movie_features = [None for i in xrange(NUM_FEATURES)];
 
 for f in xrange(NUM_FEATURES):
-	user_features[f] = [None for i in xrange(NUM_USERS)];
-	movie_features[f] = [None for i in xrange(NUM_MOVIES)];
+	user_features[f] = [0.1 for i in xrange(NUM_USERS)];
+	movie_features[f] = [0.1 for i in xrange(NUM_MOVIES)];
 
 global_avg = compute_global_avg(data);
 
 user_offset = compute_user_offset(data, global_avg);
 
-movie_avg = compute_movie_avg(data);
+# Uncomment for raw movie average
+# movie_avg = compute_movie_avg(data, global_avg);
+# Use predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id):
+# to get Version 1
+
+better_movie_avg = improved_movie_avg(data, global_avg);
+# Use predict_rating_uninitialized(user_offset, better_movie_avg, user_id, movie_id):
+# to get version 2
+
+rating_initialized = false;
+
+for i in xrange(NUM_ITERATIONS):
+	for f in xrange(NUM_FEATURES):
+		for u in xrange(NUM_USERS):
+			for i in xrange(len(data[u]) / 2):
+				train(user_features, movie_features, f, user_offset, better_movie_avg, u, data[u][2 * i] - 1, rating_initialized, data[u][2 * i + 1], LEARNING_RATE);
+	rating_initialized = true;
+
+# Learning is done at this point.
+# use predict_rating to predict ratings

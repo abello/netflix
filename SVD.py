ac82e4bd57c175bb7c4c595ceb2d73fc3e2c2918
Dae Hyun Kim <dhkim0106@gmail.com>
2014-04-13 18:12:05 -0700

    SVD cleanup code
    - Some commenting done
    - Tabs to spaces
    
    TODO
    - Printing portion

--- SVD.py
+++ SVD.py
@@ -8,127 +8,133 @@ NUM_USERS = 100;#458293;
 # Number of movies
 NUM_MOVIES = 17770;
 
-NUM_ITERATIONS = 10;
+# Number of desired iterations
+NUM_ITERATIONS = 1;
 
+# Learning rate
 LEARNING_RATE = 0.001;
 
 
+# Compute the global average
 def compute_global_avg(data):
-	print("Computing the global average...");
-
+    print("Computing the global average...");
 
 #DEBUG CODE
-	cnt = 1;
+    cnt = 1;
 #DEBUG CODE END
 
-	tot_cnt = 0;
-	tot_sum = 0;
+    tot_cnt = 0;
+    tot_sum = 0;
 
-	for user_data in data:
-		tot_cnt += len(user_data) / 2;
-		
-		for i in xrange(len(user_data) / 2):
-			tot_sum += user_data[2 * i + 1];
-		#DEBUG CODE
-		cnt += 1;
-		if (cnt == NUM_USERS):
-			break;
-		#DEBUG CODE END
+    for user_data in data:
+        tot_cnt += len(user_data) / 2;
+        
+        for i in xrange(len(user_data) / 2):
+            tot_sum += user_data[2 * i + 1];
+        #DEBUG CODE
+        cnt += 1;
+        if (cnt == NUM_USERS):
+            break;
+        #DEBUG CODE END
 
-	print("Finished computing the global average!");
-	return float(tot_sum) / float(tot_cnt);
+    print("Finished computing the global average!");
+    return float(tot_sum) / float(tot_cnt);
 
-
+# Compute the user averages
 def compute_user_avg(data):
-	print("Computing the user averages...");
+    print("Computing the user averages...");
 
-	user_avg = [None for i in xrange(NUM_USERS)];
+    user_avg = [None for i in xrange(NUM_USERS)];
 
-	for u in xrange(NUM_USERS):
-		user_cnt = len(data[u]) / 2;
-		user_sum = 0;
-		
-		for i in xrange(len(data[u]) / 2):
-			user_sum += data[u][2 * i + 1];
+    for u in xrange(NUM_USERS):
+        user_cnt = len(data[u]) / 2;
+        user_sum = 0;
+        
+        for i in xrange(len(data[u]) / 2):
+            user_sum += data[u][2 * i + 1];
 
-		user_avg[u] = float(user_sum) / float(user_cnt);
+        user_avg[u] = float(user_sum) / float(user_cnt);
 
-	print("Finished computing the user averages!");
-	return user_avg;
+    print("Finished computing the user averages!");
+    return user_avg;
 
+# Compute the movie averages
 # CAUTION! INDICES SHIFTED BY 1
 def compute_movie_avg(data):
-	print("Computing the movie averages...");
+    print("Computing the movie averages...");
 
-	movie_avg = [None for i in xrange(NUM_MOVIES)];
-	movie_cnt = [0 for i in xrange(NUM_MOVIES)];
-	movie_sum = [0 for i in xrange(NUM_MOVIES)];
+    movie_avg = [None for i in xrange(NUM_MOVIES)];
+    movie_cnt = [0 for i in xrange(NUM_MOVIES)];
+    movie_sum = [0 for i in xrange(NUM_MOVIES)];
 
-	for u in xrange(NUM_USERS):
-		for i in xrange(len(data[u]) / 2):
-			movie_no = data[u][2 * i] - 1; # shift by 1!
-			movie_cnt[movie_no] += 1;
-			movie_sum[movie_no] += data[u][2 * i + 1];
+    for u in xrange(NUM_USERS):
+        for i in xrange(len(data[u]) / 2):
+            movie_no = data[u][2 * i] - 1; # shift by 1!
+            movie_cnt[movie_no] += 1;
+            movie_sum[movie_no] += data[u][2 * i + 1];
 
-	for m in xrange(NUM_MOVIES):
-		#DEBUG CODE
-		if (movie_cnt[m] == 0):
-			movie_avg[m] = 2.5;
-			continue;
-		#DEBUG CODE END
-		movie_avg[m] = float(movie_sum[m]) / float(movie_cnt[m]);
+    for m in xrange(NUM_MOVIES):
+        #DEBUG CODE
+        if (movie_cnt[m] == 0):
+            movie_avg[m] = 2.5;
+            continue;
+        #DEBUG CODE END
+        movie_avg[m] = float(movie_sum[m]) / float(movie_cnt[m]);
 
-	print("Finished computing the movie averages!");
-	return movie_avg;
+    print("Finished computing the movie averages!");
+    return movie_avg;
 
+# Compute difference between the user average and the global average
 def compute_user_offset(data, global_avg):
-	user_avg = compute_user_avg(data);
-	user_offset = [(user_avg[i] - global_avg) for i in xrange(NUM_USERS)];
-	return user_offset;
+    user_avg = compute_user_avg(data);
+    user_offset = [(user_avg[i] - global_avg) for i in xrange(NUM_USERS)];
+    return user_offset;
 
+# Computes the movie average with weight K on global average
 def improved_movie_avg(data, global_avg, K = 25):
-	print("Computing the movie averages (improved)...");
+    print("Computing the movie averages (improved)...");
 
-	movie_avg = [None for i in xrange(NUM_MOVIES)];
-	movie_cnt = [K for i in xrange(NUM_MOVIES)];
-	movie_sum = [K * global_avg for i in xrange(NUM_MOVIES)];
+    movie_avg = [None for i in xrange(NUM_MOVIES)];
+    movie_cnt = [K for i in xrange(NUM_MOVIES)];
+    movie_sum = [K * global_avg for i in xrange(NUM_MOVIES)];
 
-	for u in xrange(NUM_USERS):
-		for i in xrange(len(data[u]) / 2):
-			movie_no = data[u][2 * i] - 1; # shift by 1!
-			movie_cnt[movie_no] += 1;
-			movie_sum[movie_no] += data[u][2 * i + 1];
+    for u in xrange(NUM_USERS):
+        for i in xrange(len(data[u]) / 2):
+            movie_no = data[u][2 * i] - 1; # shift by 1!
+            movie_cnt[movie_no] += 1;
+            movie_sum[movie_no] += data[u][2 * i + 1];
 
-	for m in xrange(NUM_MOVIES):
-		movie_avg[m] = float(movie_sum[m]) / float(movie_cnt[m]);
+    for m in xrange(NUM_MOVIES):
+        movie_avg[m] = float(movie_sum[m]) / float(movie_cnt[m]);
 
-	print("Finished computing the movie averages!");
-	return movie_avg;
+    print("Finished computing the movie averages!");
+    return movie_avg;
 
 
 # base value for predicted ratings
 def predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id):
-	#print movie_avg[movie_id] + user_offset[user_id];
-	return movie_avg[movie_id] + user_offset[user_id];
+    return movie_avg[movie_id] + user_offset[user_id];
 
+# Predict rating given the specified inputs
 def predict_rating(user_features, movie_features, user_offset, movie_avg, user_id, movie_id, rating_initialized):
-	if not rating_initialized:
-		return predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id);
-	else:
-		tot_sum = 0;
-		for f in xrange(NUM_FEATURES):
-			tot_sum += user_features[f][user_id] * movie_features[f][movie_id];
-		return tot_sum;
+    if not rating_initialized:
+        return predict_rating_uninitialized(user_offset, movie_avg, user_id, movie_id);
+    else:
+        tot_sum = 0;
+        for f in xrange(NUM_FEATURES):
+            tot_sum += user_features[f][user_id] * movie_features[f][movie_id];
+        return tot_sum;
 
+# Actually do the training!
 def train(user_features, movie_features, f, user_offset, movie_avg, user_id, movie_id, rating_initialized, actual_rating, learning_rate):
-	error = learning_rate * (actual_rating - predict_rating(user_features, movie_features, user_offset, movie_avg, user_id, movie_id, rating_initialized));
-	uv_old = user_features[f][user_id];
-	user_features[f][user_id] += error * movie_features[f][movie_id];
-	movie_features[f][movie_id] += error * uv_old;
-	return;
+    error = learning_rate * (actual_rating - predict_rating(user_features, movie_features, user_offset, movie_avg, user_id, movie_id, rating_initialized));
+    uv_old = user_features[f][user_id];
+    user_features[f][user_id] += error * movie_features[f][movie_id];
+    movie_features[f][movie_id] += error * uv_old;
+    return;
 
 
-
+# Actual program
 
 print("Reading from data.npz...");
 
@@ -142,8 +148,8 @@ user_features = [None for i in xrange(NUM_FEATURES)];
 movie_features = [None for i in xrange(NUM_FEATURES)];
 
 for f in xrange(NUM_FEATURES):
-	user_features[f] = [0.1 for i in xrange(NUM_USERS)];
-	movie_features[f] = [0.1 for i in xrange(NUM_MOVIES)];
+    user_features[f] = [0.1 for i in xrange(NUM_USERS)];
+    movie_features[f] = [0.1 for i in xrange(NUM_MOVIES)];
 
 global_avg = compute_global_avg(data);
 print global_avg;
@@ -163,15 +169,15 @@ better_movie_avg = improved_movie_avg(data, global_avg);
 rating_initialized = False;
 
 for i in xrange(NUM_ITERATIONS):
-	for f in xrange(NUM_FEATURES):
-		for u in xrange(NUM_USERS):
-			for i in xrange(len(data[u]) / 2):
-				train(user_features, movie_features, f, user_offset, movie_avg, u, data[u][2 * i] - 1, rating_initialized, data[u][2 * i + 1], LEARNING_RATE);
-	rating_initialized = True;
-	print("Iteration ", i, " completed!");
+    for f in xrange(NUM_FEATURES):
+        for u in xrange(NUM_USERS):
+            for i in xrange(len(data[u]) / 2):
+                train(user_features, movie_features, f, user_offset, movie_avg, u, data[u][2 * i] - 1, rating_initialized, data[u][2 * i + 1], LEARNING_RATE);
+    rating_initialized = True;
+    print("Iteration " + str(i) + " completed!");
 
-
-print predict_rating(user_features, movie_features, user_offset, better_movie_avg, 10, 10, rating_initialized);
 
 # Learning is done at this point.
 # use predict_rating to predict ratings
+# Such as:
+print predict_rating(user_features, movie_features, user_offset, better_movie_avg, 10, 10, rating_initialized);

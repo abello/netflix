23fe197220209a5842c7d9e9c4fd9d45332d8b76
Dae Hyun Kim <dhkim0106@gmail.com>
2014-04-20 01:51:24 -0700

    Cython Problem Fixed
    -Fixed problems with Cython compilation

--- ht.pyx
+++ ht.pyx
@@ -7,7 +7,6 @@ cdef int NUM_USERS = 458293
 
 cdef int NUM_MOVIES = 17770
 
-cdef unordered_map[int, float] *cache
 
 
     
@@ -16,31 +15,42 @@ from cython.operator cimport dereference as deref, preincrement as inc
 from libcpp.string cimport string
 from libcpp.pair cimport pair
  
-cdef extern from "<unordered_map>" namespace "stl":
-    cdef cppclass unordered_map[int, float]: # K: key_type, T: mapped_type
+cdef extern from "<unordered_map>" namespace "std":
+    cdef cppclass unordered_map[T, U]:
         cppclass iterator:
-            pair& operator*()
-            bint operator==(iterator)
-            bint operator!=(iterator)
+            pair[T, U]& operator*() nogil
+            iterator operator++() nogil
+            iterator operator--() nogil
+            bint operator==(iterator) nogil
+            bint operator!=(iterator) nogil
         unordered_map()
-        bint empty()
-        size_t size()
-        iterator begin()
-        iterator end()
-        pair emplace(K, T)
-        iterator find(K)
-        void clear()
-        size_t count(K)
-        T& operator[](K)
- 
+        unordered_map(unordered_map&)
+        U& operator[](T&) nogil
+        # unordered_map& operator=(unordered_map&)
+        U& at(T&) nogil
+        iterator begin() nogil
+        void clear() nogil
+        size_t count(T&) nogil
+        bint empty() nogil
+        iterator end() nogil
+        pair[iterator, iterator] equal_range(T&) nogil
+        void erase(iterator) nogil
+        void erase(iterator, iterator) nogil
+        size_t erase(T&) nogil
+        iterator find(T&) nogil
+        pair[iterator, bint] insert(pair[T, U]) nogil
+        iterator insert(iterator, pair[T, U]) nogil
+        void insert(input_iterator, input_iterator)
+        size_t max_size() nogil
+        void rehash(size_t)
+        size_t size() nogil
+        void swap(unordered_map&) nogil
 
-cdef unordered_map[int, float] *cache = new unordered_map[int, float]()
-
+cdef unordered_map[int, float] cache
 
 def cache_init(um_dta, user):
     cdef int len_user, j, u, m
 
-    cache = new unordered_map[int, float]()
 
 
     for u in xrange(NUM_USERS):
@@ -52,5 +62,5 @@ def cache_init(um_dta, user):
             # movie_id
             m = user[j] - 1
 
-            #cache[u * NUM_MOVIES + m] = <float>0.4
-            cache.emplace(u * NUM_MOVIES + m, <float>0.4)
+            cache[u * NUM_MOVIES + m] = <float>0.4
+            #cache.emplace(u * NUM_MOVIES + m, <float>0.4)

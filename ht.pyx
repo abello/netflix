8e5957a362e83b9e5fa7ce166c3e6c6ede2e9632
Aleksander Bello <belloaleksander@gmail.com>
2014-04-20 01:43:52 -0700

    Trying new method

--- ht.pyx
+++ ht.pyx
@@ -1,5 +1,5 @@
 #distutils: language = c++
-from libcpp.unordered_map cimport unordered_map
+#from libcpp.unordered_map cimport unordered_map
 
 # SO and google (and code samples) were used to hack this together
 
@@ -8,6 +8,33 @@ cdef int NUM_USERS = 458293
 cdef int NUM_MOVIES = 17770
 
 cdef unordered_map[int, float] *cache
+
+
+    
+
+from cython.operator cimport dereference as deref, preincrement as inc
+from libcpp.string cimport string
+from libcpp.pair cimport pair
+ 
+cdef extern from "<unordered_map>" namespace "stl":
+    cdef cppclass unordered_map[int, float]: # K: key_type, T: mapped_type
+        cppclass iterator:
+            pair& operator*()
+            bint operator==(iterator)
+            bint operator!=(iterator)
+        unordered_map()
+        bint empty()
+        size_t size()
+        iterator begin()
+        iterator end()
+        pair emplace(K, T)
+        iterator find(K)
+        void clear()
+        size_t count(K)
+        T& operator[](K)
+ 
+
+cdef unordered_map[int, float] *cache = new unordered_map[int, float]()
 
 
 def cache_init(um_dta, user):
@@ -25,5 +52,5 @@ def cache_init(um_dta, user):
             # movie_id
             m = user[j] - 1
 
-            cache[u * NUM_MOVIES + m] = <float>0.4
-    
+            #cache[u * NUM_MOVIES + m] = <float>0.4
+            cache.emplace(u * NUM_MOVIES + m, <float>0.4)

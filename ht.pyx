0e64840d11270cbe21ea02a08da80f4a746001b2
Aleksander Bello <belloaleksander@gmail.com>
2014-04-20 14:43:21 -0700

    Packing two shorts in an int

--- ht.pyx
+++ ht.pyx
@@ -11,43 +11,47 @@ cdef int NUM_MOVIES = 17770
 
 # Struct elem that is gonna be stored on each hashtable entry
 # TODO: See if we can make this a bitfield
-cdef struct s_elem:
-    unsigned short cache
-    unsigned short rating
-ctypedef s_elem elem
+#cdef packed struct s_elem:
+#    unsigned short cache
+#    unsigned char rating
+#ctypedef s_elem elem
 
 # TODO: Use reserve?
 # TODO: Set load factors
-cdef unordered_map[unsigned int, elem] cache
+cdef unordered_map[unsigned int, unsigned int] cache
 
 def cache_init(um_dta):
     cdef int len_user, j, u, m
-    cdef elem *e
+    cdef unsigned short c, r
+    #cdef elem *e
 
     for u in xrange(NUM_USERS):
         user = um_dta[u]
         len_user = len(user)
 
         # All values OBO
-        for j in range(0, len_user, 2):
-            e = <elem *> malloc(sizeof(elem))
-            
+        for j in xrange(0, len_user, 2):
+            #e = <elem *> malloc(sizeof(elem))
+
             # movie_id
             m = user[j] - 1
-            e.cache = <unsigned short> 400
-            e.rating = <unsigned short> user[j+1]
+            c = <unsigned short> 400
+            r = <unsigned short> user[j+1]
 
-            cache[u * NUM_MOVIES + m] = deref(e)
-            #cache.emplace(u * NUM_MOVIES + m, <float>0.4)
+            cache[u * NUM_MOVIES + m] = <unsigned int> ((c<<16) | (r & 0xffff))
 
 def cache_get(unsigned int movie, unsigned int user):
-    return cache[user * NUM_MOVIES + movie].cache / 100.0
+    #return cache[user * NUM_MOVIES + movie].cache / 100.0
+    return 1
 
 def cache_set(unsigned int movie, unsigned int user, double val):
-    cache[user * NUM_MOVIES + movie].cache = <unsigned short> (val * 100)
+    #cache[user * NUM_MOVIES + movie].cache = <unsigned short> (val * 100)
+    return 1
 
 def ratings_get(unsigned int movie, unsigned int user):
-    return cache[user * NUM_MOVIES + movie].rating / 100.0
+    #return cache[user * NUM_MOVIES + movie].rating
+    return 1
 
-def ratings_set(unsigned int movie, unsigned int user, double val):
-    cache[user * NUM_MOVIES + movie].rating = <unsigned short> (val * 100)
+def ratings_set(unsigned int movie, unsigned int user, unsigned char val):
+    return 1
+    #cache[user * NUM_MOVIES + movie].rating = val

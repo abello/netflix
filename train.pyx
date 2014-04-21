82f6328d0cfb358b4ea972e17e4f059fd537409a
Aleksander Bello <belloaleksander@gmail.com>
2014-04-20 18:30:53 -0700

    Profiling train

--- train.pyx
+++ train.pyx
@@ -4,6 +4,7 @@ from cython.operator cimport dereference as deref
 import numpy as np
 cimport numpy as np
 from ht import cache_get, cache_set, ratings_get
+import time
 
 # SO and google (and code samples) were used to hack this together
 
@@ -25,16 +26,20 @@ def loop(data, np.ndarray[np.float32_t, ndim=1] user_ofsts, np.ndarray[np.float3
     cdef int actual_rating
     cdef float error, uv_old
     cdef int len_data_user
+    cdef int _users = 0
+    cdef float _sum = 0
 
     for i in xrange(NUM_ITERATIONS):
         for f in xrange(NUM_FEATURES):
             uf = user_features[f]
             mf = movie_features[f]
             for user in xrange(NUM_USERS):
+                start = time.time()
                 len_data_user = len(data[user])/2
+                user_off = user_ofsts[user]
+
                 for j in xrange(len_data_user):
                     movie = data[user][2 * j] - 1
-                    user_off = user_ofsts[user]
                     movie_avg = movie_avgs[movie]
 
                     # Rating we currently have
@@ -51,4 +56,11 @@ def loop(data, np.ndarray[np.float32_t, ndim=1] user_ofsts, np.ndarray[np.float3
                     
                     # Update cache
                     cache_set(movie, user, cache_get(movie, user) - tmp + uf[user] * mf[movie])
+
+                _sum += time.time() - start
+                _users += 1
+                if (user % 1000) == 0:
+                    print "avg for 1000 users", _sum/_users
+                    _sum = 0
+                    _users =0
         print "Finished iteration %d", i

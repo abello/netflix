635ed0d019a2378c0b8dada07c4603a51ab9c904
Aleksander Bello <belloaleksander@gmail.com>
2014-04-21 00:18:30 -0700

    Make verifier check for approximately correct (due to floating point precision)

--- verify_cache.py
+++ verify_cache.py
@@ -1,5 +1,6 @@
 import numpy as np
-from ht import cache_init, cache_get, cache_set, ratings_get, ratings_set
+from ht import cache_init, cache_get, cache_set
+import math
 
 if __name__ == '__main__':
     f = open('data.npz', 'r')
@@ -15,9 +16,8 @@ if __name__ == '__main__':
     for i in xrange(NUM_USERS):
         user = i
         for j in xrange(0, len(um_dta[i]), 2):
-            movie = um_dta[j] - 1
+            movie = um_dta[user][j]  - 1
             rating = um_dta[i][j + 1]
-            assert (rating == ratings_get(movie, user))
-            assert (cache_get(movie, user) == 0.4)
+            assert (abs(cache_get(movie, user) - 0.4) < 0.0001)
     print "Cache was initialized properly!"
     f.close()

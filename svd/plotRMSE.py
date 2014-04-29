fc66dce58e5684f87b4091db9d7152c10724c723
Ronnel Boettcher <ronnel132@gmail.com>
2014-04-29 10:35:01 -0700

    Added axes for plotRMSE

--- svd/plotRMSE.py
+++ svd/plotRMSE.py
@@ -3,5 +3,7 @@ import matplotlib.pyplot as plt
 rmses = [float(v) for v in open('rmseOut.txt', 'r').readlines()]
 features = range(50)
 
-plt.plot(features, rmses)
+plt.plot(features, rmses, 'ro')
+plt.xlabel('Number of Features')
+plt.ylabel('RMSE')
 plt.show()

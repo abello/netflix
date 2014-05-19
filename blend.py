78c38e4820ef777b8cd42a0caea8112b64603474
Aleksander Bello <belloaleksander@gmail.com>
2014-05-19 13:58:52 -0700

    Current best blend: 5.16% above water

--- blend.py
+++ blend.py
@@ -5,11 +5,12 @@ f20_2 = open("results/output-F=20-E=80,180-k=0.02-l=0.001-SC-E=0-SCC=0-NBINS=5",
 f50_1 =  open("results/output-F=50-E=120,180-k=0.015-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
 f100_1 = open("results/output-F=100-E=120,180-k=0.015-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
 f100_2 = open("results/output-F=100-E=120,180-k=0.02-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
-f_GC_07 = open("results/output-GC-0.07", "r")
+# f_GC_07 = open("results/output-GC-0.07", "r")
+f_GC_13 = open("results/output-GC-0.13", "r")
 
 
 
-files = [f20_1, f20_2, f50_1, f100_1, f_GC_07]
+files = [f20_1, f20_2, f50_1, f100_1, f100_2, f_GC_13]
 
 out = open("results/blended", "w")
 
@@ -18,9 +19,11 @@ wf_20_2 = 0.440380010101
 wf_50_1 = 0.224305705105
 wf_100_1 = 0.211711841455
 wf_100_2 = 0.211711841455
-wf_GC_07 = 0.223826567397
+wf_GC_13 = 0.223826567397
 
-weights = [wf_20_1, wf_20_2, wf_50_1, wf_100_1, wf_100_2, wf_GC_07]
+weights = [wf_20_1, wf_20_2, wf_50_1, wf_100_1, wf_100_2, wf_GC_13]
+
+assert(len(weights) == len(files))
 
 for i in xrange(QUAL_SIZE):
     j = 0

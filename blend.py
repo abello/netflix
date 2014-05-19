c480cdfbf0ec25992ce73f3c67e19d73627cd39a
Aleksander Bello <belloaleksander@gmail.com>
2014-05-18 23:42:27 -0700

    Modularized blend.py. Remember to fix WRONG file for f=100

--- blend.py
+++ blend.py
@@ -1,18 +1,32 @@
 QUAL_SIZE = 2749898
 
-f5 =  open("results/output-F=5-E=20,20-k=0.015-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
-f10 = open("results/output-F=10-E=15,15-k=0.015-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
+# TODO: Fix mismatch in f100_1!!!!
+
+f20_1 =  open("results/output-F=20-E=80,180-k=0.015-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
+f20_2 = open("results/output-F=20-E=80,180-k=0.02-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
+f50_1 =  open("results/output-F=50-E=120,180-k=0.015-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
+f100_1 = open("results/output-F=100-E=120,180-k=0.02-l=0.001-SC-E=0-SCC=0-NBINS=5", "r")
+
+files = [f20_1, f20_2, f50_1, f100_1]
 
 out = open("results/blended", "w")
 
-w5 = 1.4085301516
-w10 = -0.4181661914
+
+w20_1 = -0.235276048724
+w20_2 = 0.473523895167
+w50_1 = 0.277299341734
+w100_1 = 0.480955148007
+
+weights = [w20_1, w20_2, w50_1, w100_1]
 
 for i in xrange(QUAL_SIZE):
-    l5 = f5.readline().rstrip()
-    l10 = f10.readline().rstrip()
-    r5 = float(l5)
-    r10 = float(l10)
-    b = round(r5 * w5 + r10 * w10, 4)
+    j = 0
+    b = 0
+    for f in files:
+        val = f.readline().rstrip()
+        b += float(val) * weights[j]
+        j += 1
+    b = round(b, 4)
+
     out.write(str(b) + "\n")
 
